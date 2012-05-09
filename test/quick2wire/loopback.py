f7f6896d21317e3860951e5905b152601e47605d
romilly <romilly@quick2wire.com>
2012-05-09 11:16:49 +0100

    more refactoring of loopback test

--- test/quick2wire/loopback.py
+++ test/quick2wire/loopback.py
@@ -7,23 +7,23 @@ class LoopbackTest(unittest.TestCase):
         self.pins = list([Pin(headerPinNumber) for headerPinNumber in [11, 12, 13, 15, 16, 18, 22, 7]])
         for pin in self.pins:
             pin.export()
-		
+        
     def tearDown(self):
         for pin in self.pins:
             if pin.is_exported:
                 pin.unexport()
 		
     def test_GPIO(self):
-        for i in range(0, 4):
-            self.pins[i].direction = Pin.Out
-            self.pins[i+4].direction = Pin.In
-            self.checkOutputSeeenAtInput(self.pins[i], self.pins[i+4])
-        for i in range(0, 4):
-            self.pins[i].direction = Pin.In
-            self.pins[i+4].direction = Pin.Out
-            self.checkOutputSeeenAtInput(self.pins[i+4], self.pins[i])
+        self.checkOutputsSeenAtCorrespondingInputs(self.pins[:4], self.pins[4:])
+        self.checkOutputsSeenAtCorrespondingInputs(self.pins[4:], self.pins[:4])
         
-    def checkOutputSeeenAtInput(self, op, ip):
+    def checkOutputsSeenAtCorrespondingInputs(self, outputs, inputs):
+        for (op, ip) in zip(outputs, inputs):
+            op.direction = Pin.Out
+            ip.direction = Pin.In
+            self.checkOutputSeenAtInput(op, ip)
+        
+    def checkOutputSeenAtInput(self, op, ip):
         for value in [0, 1]:
             op.value = value
             self.assertEqual(ip.value, value, 'input %d was not %d' % (ip.header_pin_id, value))
