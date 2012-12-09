1a9f028abfd6b17bb466d14531c3d45b5297290a
Romilly Cocking <romilly@quick2wire.com>
2012-12-09 17:35:12 +0000

    added display helper

--- examples/analogue-bar.py
+++ examples/analogue-bar.py
@@ -1,20 +1,14 @@
-from time import sleep
 from contextlib import closing
 from quick2wire.gpio import Pin
+from quick2wire.helpers.display import AnalogueDisplay
 import quick2wire.i2c as i2c
-
 from quick2wire.parts.pcf8591 import PCF8591
-
-ANALOGUE_IN = 0
-
-def display(pins, value):
-    for count in range(8):
-        pins[count].value = 1 if value < 8 * count else 0
 
 with closing(i2c.I2CMaster()) as master:
     pcf = PCF8591(master)
     pins = [Pin(header_pin_number, Pin.Out) for header_pin_number in [11, 12, 13, 15, 16, 18, 22, 7]]
-    pin = pcf.begin_analogue_read(ANALOGUE_IN)
+    display = AnalogueDisplay(64, *pins)
+    pin = pcf.begin_analogue_read(0)
     while(True):
-        display(pins, pin.read())
+        display.display(pin.read())
 
