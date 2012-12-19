ec874ff9903940d65c1833cbb5072a37592cb1a9
Nat Pryce <sw@natpryce.com>
2012-12-19 18:13:57 +0000

    better testing of packaging and deployment via setup.py

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-setup(name='Quick2Wire',
+setup(name='quick2wire-api',
       version='0.0.0.2',
-      description='Quick2Wire API',
+      description='Quick2Wire API for Physical Computing',
       long_description=contents_of('README.txt'),
       author='Quick2Wire Ltd.',
       author_email='info@quick2wire.com',
