1c4b10c8c11d7f9554752e43231f30d406a28b68
Nat Pryce <sw@natpryce.com>
2013-01-15 22:42:06 +0000

    a simple selector that makes epoll more convenient

---
+++ quick2wire/selector.py
@@ -0,0 +1,56 @@
+
+
+import select
+
+INPUT = select.EPOLLIN
+OUTPUT = select.EPOLLOUT
+ERROR = select.EPOLLERR
+HANGUP = select.EPOLLHUP
+PRIORITY_INPUT = select.EPOLLPRI
+
+LEVEL = 0
+EDGE = 1
+
+
+class Selector:
+    def __init__(self, size_hint=-1):
+        self._epoll = select.epoll(size_hint)
+        self._sources = {}
+        self.ready = None
+        self.events = 0
+        
+    def fileno(self):
+        return self._epoll.fileno()
+    
+    def add(self, source, eventmask, trigger=LEVEL, identifier=None):
+        fileno = source.fileno()
+        self._sources[fileno] = identifier if identifier is not None else source
+        self._epoll.register(fileno, eventmask|(select.EPOLLET*trigger))
+    
+    def wait(self, timeout=-1):
+        fileno, self.events = self._epoll.poll(timeout, maxevents=1)[0]
+        self.ready = self._sources[fileno]
+        
+    @property
+    def has_input(self):
+        return bool(self.events & INPUT)
+    
+    @property
+    def has_output(self):
+        return bool(self.events & OUTPUT)
+    
+    @property
+    def has_error(self):
+        return bool(self.events & ERROR)
+    
+    @property
+    def has_hangup(self):
+        return bool(self.events & HANGUP)
+    
+    @property
+    def has_priority_input(self):
+        return bool(self.events & PRIORITY_INPUT)
+    
+    def close(self):
+        self._epoll.close()
+        
---
+++ quick2wire/test_selector.py
@@ -0,0 +1,59 @@
+
+from contextlib import closing
+from itertools import islice
+from quick2wire.eventfd import Semaphore
+from quick2wire.selector import Selector, INPUT, OUTPUT, ERROR
+
+
+def test_selector_is_a_convenient_api_to_epoll():
+    with closing(Semaphore(blocking=False)) as ev1, \
+         closing(Selector()) as selector:
+        
+        selector.add(ev1, INPUT)
+        
+        ev1.signal()
+        
+        selector.wait()
+        assert selector.ready == ev1
+        assert selector.has_input == True
+        assert selector.has_output == False
+        assert selector.has_error == False
+        assert selector.has_hangup == False
+        assert selector.has_priority_input == False
+
+        
+def test_selecting_from_multiple_event_sources():
+    with closing(Semaphore(blocking=False)) as ev1, \
+         closing(Semaphore(blocking=False)) as ev2, \
+         closing(Selector()) as selector:
+        
+        selector.add(ev1, INPUT)
+        selector.add(ev2, INPUT)
+        
+        ev1.signal()
+        ev2.signal()
+        
+        selector.wait()
+        first = selector.ready
+        first.receive()
+        
+        selector.wait()
+        second = selector.ready
+        second.receive()
+        
+        assert first in (ev1, ev2)
+        assert second in (ev1, ev2)
+        assert first is not second
+        
+        
+def test_can_use_a_different_value_to_identify_the_event_source():
+    with closing(Semaphore(blocking=False)) as ev1, \
+         closing(Selector()) as selector:
+        
+        selector.add(ev1, INPUT, identifier=999)
+        
+        ev1.signal()
+        
+        selector.wait()
+        assert selector.ready == 999
+
