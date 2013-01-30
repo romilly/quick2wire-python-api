6902a10ed5531ad8a4565e15cdf09a5869231c30
Nat Pryce <sw@natpryce.com>
2013-01-30 22:58:55 +0000

    loopback test runs against new API, deleted old loopback example

--- quick2wire/parts/test_pcf8591_loopback.py
+++ quick2wire/parts/test_pcf8591_loopback.py
@@ -1,7 +1,8 @@
+
 from time import sleep
+from quick2wire.i2c import I2CMaster
+from quick2wire.parts.pcf8591 import PCF8591, FOUR_SINGLE_ENDED
 import pytest
-import quick2wire.i2c as i2c
-from quick2wire.parts.pcf8591 import PCF8591
 
 
 # simple test for now - loops back analogue output to analogue input 1
@@ -10,13 +11,14 @@ ANALOGUE_IN = 1
 @pytest.mark.loopback
 @pytest.mark.pcf8591
 def test_pcf8591_loopback():
-    with i2c.I2CMaster() as master:
-        chip = PCF8591(master)
-        pin = chip.begin_analogue_read(ANALOGUE_IN)
+    with I2CMaster() as i2c:
+        adc = PCF8591(i2c, FOUR_SINGLE_ENDED)
+        input = adc.input_pin(ANALOGUE_IN)
         
-        for output in range(256):
-            chip.analogue_out(output)
-            sleep(0.05)
-            input = pin.read()
-            
-            assert abs(output - input) < 4
+        with adc.output_pin as output:
+            for i in range(256):
+                v = i/255.0
+                
+                output.value = v
+                
+                assert abs(input.value - v) < 0.05
