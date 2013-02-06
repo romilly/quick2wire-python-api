0cced72f2bc30d5ccaa8ad26070bbfef1961f4bf
Nat Pryce <sw@natpryce.com>
2013-02-06 20:37:38 +0000

    first stab at loopback test for differential channels

--- quick2wire/parts/test_pcf8591_loopback.py
+++ quick2wire/parts/test_pcf8591_loopback.py
@@ -1,3 +1,13 @@
+"""Loopback tests for the PCF8591 API
+
+Topology:
+
+ - connect AOUT to AIN3
+ - connect AIN1 to GND
+ - connect AIN2 to VREF
+ - AIN0 is unused
+
+"""
 
 from time import sleep
 from quick2wire.i2c import I2CMaster
@@ -5,20 +15,42 @@ from quick2wire.parts.pcf8591 import PCF8591, FOUR_SINGLE_ENDED
 import pytest
 
 
-# simple test for now - loops back analogue output to analogue input 1
-ANALOGUE_IN = 1
+def before_function(f):
+    global i2c
+    i2c = I2CMaster()
+
+
+def after_function(f):
+    i2c.close()
+
+
+def assert_is_approx(expected, value, delta=0.05):
+    assert abs(value - expected) <= delta
+
 
 @pytest.mark.loopback
 @pytest.mark.pcf8591
-def test_pcf8591_loopback():
-    with I2CMaster() as i2c:
-        adc = PCF8591(i2c, FOUR_SINGLE_ENDED)
-        input = adc.single_ended_input(ANALOGUE_IN)
-        
-        with adc.output as output:
-            for i in range(256):
-                v = i/255.0
-                
-                output.value = v
-                
-                assert abs(input.value - v) < 0.05
+def test_pcf8591_loopback_single_ended():
+    adc = PCF8591(i2c, FOUR_SINGLE_ENDED)
+    input = adc.single_ended_input(3)
+    
+    with adc.output as output:
+        for i in range(256):
+            v = i/255.0
+            output.value = v
+            assert_is_approx(v, input.value)
+
+
+@pytest.mark.loopback
+@pytest.mark.pcf8591
+def test_pcf8591_loopback_differential():
+    adc = PCF8591(i2c, THREE_DIFFERENTIAL)
+    cmp_gnd = adc.differential_input(1)
+    cmp_vref = adc.differential_input(2)
+    
+    with adc.output as output:
+        for i in range(256):
+            v = i/255.0
+            output.value = v
+            assert_is_approx(v, cmp_gnd.value)
+            assert_is_approx(v-1, cmp_vref.value)
