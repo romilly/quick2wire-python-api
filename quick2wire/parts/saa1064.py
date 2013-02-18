96ee4f1efce502d479f5d67a83ec943430c02bf2
Stuart Ervine <spervine@gmail.com>
2013-02-18 22:30:34 +0000

    Adding write tests for first 8 segment outputs. Need to think about the PinBank model for this. [SE]

---
+++ quick2wire/parts/saa1064.py
@@ -0,0 +1,69 @@
+from functools import reduce
+from operator import or_
+
+__author__ = 'stuartervine'
+
+from quick2wire.i2c import writing_bytes
+
+displayController = 0x38
+STATIC_MODE = 0b00000000
+DYNAMIC_MODE = 0b01000000
+
+class SAA1064(object):
+    def __init__(self, master):
+        self.master = master
+        self._mode = STATIC_MODE
+        self._brightness = 7
+        self._segment_output = tuple(_OutputPin(i) for i in range(8))
+
+    def display(self, digits):
+        pass
+
+    def configure(self):
+        self.master.transaction(
+            writing_bytes(displayController, 0b00000000, self.mode | self.brightness)
+        )
+
+    def write(self):
+        bits_to_write = [output.asBinary for output in self._segment_output]
+        byte_to_write = reduce(or_, bits_to_write)
+        self.master.transaction(
+            writing_bytes(displayController, 0b00000001, byte_to_write)
+        )
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+
+    @property
+    def brightness(self):
+        return self._brightness
+
+    @brightness.setter
+    def brightness(self, value):
+        self._brightness = value
+
+    def segment_output(self, index):
+        return self._segment_output[index]
+
+
+class _OutputPin(object):
+    def __init__(self, index):
+        self._value = 0
+        self._binary = 2 ** index
+
+    @property
+    def asBinary(self):
+        return self._value * self._binary
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
---
+++ quick2wire/parts/test_saa1064.py
@@ -0,0 +1,103 @@
+from quick2wire.i2c_ctypes import I2C_M_RD
+from quick2wire.parts.saa1064 import SAA1064, STATIC_MODE, DYNAMIC_MODE
+
+__author__ = 'stuartervine'
+
+
+class FakeI2CMaster:
+    def __init__(self):
+        self._requests = []
+        self._responses = []
+        self._next_response = 0
+        self.message_precondition = lambda m: True
+
+    def all_messages_must(self, p):
+        self.message_precondition
+
+    def clear(self):
+        self.__init__()
+
+    def transaction(self, *messages):
+        for m in messages:
+            self.message_precondition(m)
+
+        self._requests.append(messages)
+        return []
+
+    def add_response(self, *messages):
+        self._responses.append(messages)
+
+
+    @property
+    def request_count(self):
+        return len(self._requests)
+
+
+    def request(self, n):
+        return self._requests[n]
+
+
+i2c = FakeI2CMaster()
+
+def setup_function(f):
+    i2c.clear()
+
+def test_static_display_brightest_by_default():
+    saa1064 = SAA1064(i2c)
+    saa1064.configure()
+
+    assert i2c.request_count == 1
+
+    controlMessage = i2c.request(0)[0]
+    assert controlMessage.len == 2
+    assert controlMessage.buf[0][0] == 0b00000000
+    assert controlMessage.buf[1][0] == 0b00000111
+
+
+def test_dynamic_display_brightest_by_default():
+    saa1064 = SAA1064(i2c)
+    saa1064.mode=DYNAMIC_MODE
+    saa1064.configure()
+
+    assert i2c.request_count == 1
+
+    controlMessage = i2c.request(0)[0]
+    assert controlMessage.len == 2
+    assert controlMessage.buf[0][0] == 0b00000000
+    assert controlMessage.buf[1][0] == 0b01000111
+
+def test_display_brightness():
+    saa1064 = SAA1064(i2c)
+    saa1064.mode=DYNAMIC_MODE
+    saa1064.brightness=5
+    saa1064.configure()
+
+    assert i2c.request_count == 1
+
+    controlMessage = i2c.request(0)[0]
+    assert controlMessage.len == 2
+    assert controlMessage.buf[0][0] == 0b00000000
+    assert controlMessage.buf[1][0] == 0b01000101
+
+def test_setting_pins_and_writing_outputs_to_i2c():
+    saa1064 = SAA1064(i2c)
+    saa1064.mode=STATIC_MODE
+    saa1064.brightness=5
+    saa1064.configure()
+
+    saa1064.segment_output(0).value=1
+    saa1064.segment_output(1).value=0
+    saa1064.segment_output(2).value=0
+    saa1064.segment_output(3).value=1
+    saa1064.segment_output(4).value=1
+    saa1064.segment_output(5).value=1
+    saa1064.segment_output(6).value=0
+    saa1064.segment_output(7).value=1
+
+    saa1064.write()
+
+    assert i2c.request_count == 2
+    dataMessage = i2c.request(1)[0]
+    assert dataMessage.len == 2
+    assert dataMessage.buf[0][0] == 0b00000001
+    assert dataMessage.buf[1][0] == 0b10111001
