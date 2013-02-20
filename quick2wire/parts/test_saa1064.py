e3e96eec5e829f0fe7e000587a9fbf65de5b5448
Stuart Ervine <spervine@gmail.com>
2013-02-20 22:13:27 +0000

    Added support for multiple pin banks (correspondng to digits) on SAA1064. [SE]

--- quick2wire/parts/test_saa1064.py
+++ quick2wire/parts/test_saa1064.py
@@ -36,6 +36,17 @@ class FakeI2CMaster:
     def request(self, n):
         return self._requests[n]
 
+    def message(self, request_index, message_index):
+        request = self.request(0)
+        return I2CMessageWrapper(request[request_index][message_index])
+
+class I2CMessageWrapper:
+    def __init__(self, i2_message):
+        self._i2c_message = i2_message
+        self.len = i2_message.len
+
+    def byte(self, index):
+        return self._i2c_message.buf[index][0]
 
 i2c = FakeI2CMaster()
 
@@ -79,8 +90,8 @@ def test_configuring_display_brightness():
     assert controlMessage.buf[0][0] == 0b00000000
     assert controlMessage.buf[1][0] == 0b01100111
 
-def test_writing_first_bank_segment_outputs_to_i2c():
-    saa1064 = SAA1064(i2c)
+def test_writing_single_digit_segment_outputs_to_i2c():
+    saa1064 = SAA1064(i2c, digits=1)
 
     saa1064.pin_bank(0).segment_output(0).value=1
     saa1064.pin_bank(0).segment_output(1).value=0
@@ -93,7 +104,41 @@ def test_writing_first_bank_segment_outputs_to_i2c():
     saa1064.write()
 
     assert i2c.request_count == 1
-    dataMessage = i2c.request(0)[0]
-    # assert dataMessage.len == 2
-    assert dataMessage[0].buf[0][0] == 0b00000001
-    assert dataMessage[0].buf[1][0] == 0b10111001
+    dataMessage = i2c.message(0, 0)
+    assert dataMessage.len == 2
+    assert dataMessage.byte(0) == 0b00000001
+    assert dataMessage.byte(1) == 0b10111001
+
+def test_writing_two_digit_segment_outputs_to_i2c():
+    saa1064 = SAA1064(i2c, digits=2)
+
+    saa1064.pin_bank(0).segment_output(0).value=1
+    saa1064.pin_bank(0).segment_output(1).value=0
+    saa1064.pin_bank(0).segment_output(2).value=1
+    saa1064.pin_bank(0).segment_output(3).value=0
+    saa1064.pin_bank(0).segment_output(4).value=0
+    saa1064.pin_bank(0).segment_output(5).value=0
+    saa1064.pin_bank(0).segment_output(6).value=0
+    saa1064.pin_bank(0).segment_output(7).value=0
+
+    saa1064.pin_bank(1).segment_output(0).value=0
+    saa1064.pin_bank(1).segment_output(1).value=0
+    saa1064.pin_bank(1).segment_output(2).value=0
+    saa1064.pin_bank(1).segment_output(3).value=0
+    saa1064.pin_bank(1).segment_output(4).value=1
+    saa1064.pin_bank(1).segment_output(5).value=0
+    saa1064.pin_bank(1).segment_output(6).value=1
+    saa1064.pin_bank(1).segment_output(7).value=0
+    saa1064.write()
+
+    assert i2c.request_count == 1
+
+    message1 = i2c.message(0, 0)
+    assert message1.len == 2
+    assert message1.byte(0) == 0b00000001
+    assert message1.byte(1) == 0b00000101
+
+    message2 = i2c.message(0, 1)
+    assert message2.len == 2
+    assert message2.byte(0) == 0b00000010
+    assert message2.byte(1) == 0b01010000
