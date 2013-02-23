2538c95b593f04ad233d206f3157eae4151a8e05
Stuart Ervine <spervine@gmail.com>
2013-02-23 14:48:53 +0000

    Added some documentation to the SAA1064 class. [SE]

--- quick2wire/parts/saa1064.py
+++ quick2wire/parts/saa1064.py
@@ -1,3 +1,63 @@
+"""
+API for the SAA1064 seven segment display driver chip.
+
+The SAA1064 can drive up to 4 seven segment displays simultaneously.
+There are 2 modes the chip can run in:
+
+STATIC mode - drives 2 seven segment displays.
+Where no multiplexing is required, and the outputs of the pins 3-11
+and 14-22 can be connected directly to the LED displays.
+[URL]
+
+DYNAMIC mode - drives 4 seven segment displays.
+The chip multiplexes the output of displays 1+3 and 2+4. The pairs
+of displays are connected to the same output pins as above. The circuit
+requires 2 transistors to be connected to the multiplex outputs to control
+which display to trigger.
+
+Applications talk to the chip via objects of the SAA1064 class. A
+SAA1064 object is created with an I2CMaster and a number of digits between 1-4.
+
+The display brightness can be supplied using the brightness property.
+This supports values between 0-7.
+
+The displays can then be controlled individually by setting valid values
+on digits and then calling write().
+
+For example:
+
+    with I2CMaster() as i2c:
+        saa1064 = SAA1064(i2c, digits=2)
+
+        saa1064.digit(0).value('1')
+        saa1064.digit(1).value('2')
+        saa1064.write()
+
+The current values supported for a digit are: 0-9, A-F, r
+
+Specifying a decimal point after the value will light the point in the display.
+
+For example:
+    with I2CMaster() as i2c:
+        saa1064 = SAA1064(i2c, digits=2)
+
+        saa1064.digit(0).value('1.')
+
+The displays are configured using the following value conversions for the display segments
+
+         --64--
+        |      |
+        2      32
+        |      |
+         -- 1--
+        |      |
+        4      16
+        |      |
+         -- 8--   DP-128
+
+"""
+
+
 __author__ = 'stuartervine'
 
 from functools import reduce
@@ -11,19 +71,7 @@ CONTINUOUS_DISPLAY = 0b00000110
 
 DECIMAL_POINT = 0b10000000
 
-"""
-    Value conversions for the display segments
 
-         --64--
-        |      |
-        2      32
-        |      |
-         -- 1--
-        |      |
-        4      16
-        |      |
-         -- 8--   DP-128
-    """
 digit_map = {
     '0':126,
     '1':48,
@@ -48,9 +96,9 @@ class SAA1064(object):
     def createPinBank(self, i):
         return _PinBank(i)
 
-    def __init__(self, master, digits=1):
+    def __init__(self, master, digits=1, brightness=7):
         self.master = master
-        self._brightness = 0b11100000
+        self.brightness = brightness
         if digits <= 0 or digits > 4:
             raise ValueError('SAA1064 only supports driving 1 to 4 digits')
         elif digits <= 2:
@@ -60,48 +108,55 @@ class SAA1064(object):
 
         self._pin_bank = tuple(self.createPinBank(i) for i in range(digits))
 
-    def display(self, digits):
-        pass
-
     def configure(self):
+        """Writes the configured control byte to the chip."""
         self.write_control(self.mode|self._brightness|CONTINUOUS_DISPLAY)
 
     def write_control(self, control_byte):
+        """Writes a raw control byte to the chip."""
         self.master.transaction(
             writing_bytes(displayController, 0b00000000, control_byte)
         )
 
     def write(self):
+        """Writes the segment outputs to the chip."""
         i2c_messages = [pin_bank.i2c_message for pin_bank in self._pin_bank]
         self.master.transaction(*i2c_messages)
 
     @property
     def mode(self):
+        """Returns the display mode the chip is currently configured in, 0: static, 1: dynamic"""
         return self._mode
 
     @mode.setter
     def mode(self, mode):
+        """Changes the display mode the chip is configured in."""
         if mode > 1 or mode < 0:
             raise ValueError('invalid mode ' + str(mode) + ' only STATIC_MODE and DYNAMIC_MODE are supported')
         self._mode = mode
 
     @property
     def brightness(self):
+        """Returns the current brightness level configured for the LED displays."""
         return self._brightness >> 5
 
     @brightness.setter
     def brightness(self, brightness):
+        """Sets the brightness of the LED displays, between 0-7."""
         if brightness > 7:
             raise ValueError('invalid brightness, valid between 0-7.')
         self._brightness = brightness << 5
 
     def digit(self, index):
+        """Returns a Digit object for the display at given 'index'."""
         return Digit(self._pin_bank[index])
 
     def pin_bank(self, index):
+        """Returns a PinBank object for the display at given 'index'."""
         return self._pin_bank[index]
 
     def __getitem__(self, n):
+        """Allows the SAA1064 to iterate through it's pin banks."""
         if 0 < n < len(self):
             raise ValueError('no pin bank index {n} out of range', n=n)
         return self._pin_bank[n]
