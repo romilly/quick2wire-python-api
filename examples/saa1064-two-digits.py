0f354269ba51c217cca83116dd68331b4808c45d
Stuart Ervine <spervine@gmail.com>
2013-03-04 08:35:07 +0000

    Updating examples for SAA1064 and SevenSegmentDisplay, removing unnecessary calls. [SE]

--- examples/saa1064-two-digits.py
+++ examples/saa1064-two-digits.py
@@ -3,9 +3,7 @@ from quick2wire.parts.saa1064 import SAA1064, STATIC_MODE
 from time import sleep
 
 saa1064 = SAA1064(i2c.I2CMaster(), digits=2)
-saa1064.mode=STATIC_MODE
 saa1064.brightness=0b01100000
-saa1064.configure()
 
 for y in range(10):
         saa1064.digit(0).value(y)
--- examples/seven_segment_display_with_two_digits.py
+++ examples/seven_segment_display_with_two_digits.py
@@ -5,12 +5,13 @@ from quick2wire.parts.saa1064 import SAA1064
 from quick2wire.parts.seven_segment_display import SevenSegmentDisplay
 from time import sleep
 
-saa1064 = SAA1064(i2c.I2CMaster(), digits=2)
-saa1064.configure()
-
-#TODO: Write a reset method.
-
+saa1064 = SAA1064(i2c.I2CMaster(), digits=4)
 sevenSegmentDisplay=SevenSegmentDisplay(saa1064)
+
 sevenSegmentDisplay.display('1.5')
 sleep(1)
-sevenSegmentDisplay.display('9R')
+sevenSegmentDisplay.display('100R')
+sleep(1)
+
+for i in range(9999):
+    sevenSegmentDisplay.display(i)
