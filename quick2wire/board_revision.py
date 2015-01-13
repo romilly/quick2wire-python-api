c7ebb352989669265694e042098bbf60a2c0ad14
Cooper Benson <skycoop@gmail.com>
2015-01-13 14:40:14 -0700

    Add B+ version to the revision checker
    The number returned is wrong considering the B+ is technically in it's
    first rev, but I don't know what to put instead considering that there
    are 1st rev B's supported by this library

--- quick2wire/board_revision.py
+++ quick2wire/board_revision.py
@@ -3,9 +3,17 @@ def revision():
         with open('/proc/cpuinfo','r') as f:
             for line in f:
                 if line.startswith('Revision'):
-                    return 1 if line.rstrip()[-1] in ['2','3'] else 2
+                    code = line.rstrip()[-4:-1]
+                    if code in ['0002','0003']:
+                        return 1
+                    elif code is '0010'
+                    # Attempting to catch the B+. Technically wrong number, but
+                    # I don't know what to put considering the B+ is rev 1 which
+                    # conflicts with the 1st rev B's...
+                        return 3
+                    else:
+                        return 2
             else:
                 return 0
     except:
         return 0
-
